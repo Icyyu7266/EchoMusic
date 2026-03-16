4175cd88371412ce50a88e55eb75fdc7c7692fa3
Icy <3510725238@qq.com>
2026-03-16 23:13:32 +0800

    Add TimeUtils class for formatting time
    进度条

---
+++ lib/utils/play_progress_bar.dart
@@ -0,0 +1,7 @@
+class TimeUtils {
+  static String formatTime(int seconds) {
+    int minutes = seconds ~/ 60;
+    int secs = seconds % 60;
+    return "$minutes:${secs.toString().padLeft(2, '0')}";
+  }
+}
